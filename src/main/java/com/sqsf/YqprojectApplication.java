eed875de136383db1a133b782d4c779309310098
guozhixing813 <19990813Gzx@>
2020-10-27 19:08:21 +0800

    接口1 赵晓凤：大屏最顶端信息：返校人数 外省返校人数 境外返校人数 重点观察人数 接口3、赵晓凤：、重点观察人员：姓名、学院、来源地、来源地确诊人数

--- src/main/java/com/sqsf/YqprojectApplication.java
+++ src/main/java/com/sqsf/YqprojectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration
+
 @SpringBootApplication
 @MapperScan("com.sqsf.mapper")
 public class YqprojectApplication {
